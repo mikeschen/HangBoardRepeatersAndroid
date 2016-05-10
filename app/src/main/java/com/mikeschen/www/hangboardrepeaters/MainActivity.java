5508f45e46f9b154b73ee6ad36e0a2af140716fe
Michael Schennum <mikeschenphoto@gmail.com>
2016-05-10 12:33:14 -0700

    adds timers

---
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/MainActivity.java
@@ -0,0 +1,34 @@
+package com.mikeschen.www.hangboardrepeaters;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.startButton)
+    Button mStartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mStartButton.setOnClickListener(this);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "Bebas.ttf");
+        mStartButton.setTypeface(custom_font);
+
+    }
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, TimerActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/TimerActivity.java
@@ -0,0 +1,71 @@
+package com.mikeschen.www.hangboardrepeaters;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class TimerActivity extends AppCompatActivity {
+
+    TextView timerTextView;
+    long startTime = 0;
+
+    //runs without a timer by reposting this handler at the end of the runnable
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            if (seconds == 10) {
+                Log.d("thats 10", seconds + "time");
+            }
+            timerTextView.setText(String.format("%d:%02d", minutes, seconds));
+
+
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+
+        timerTextView = (TextView) findViewById(R.id.text);
+
+        Button b = (Button) findViewById(R.id.startButton);
+        b.setText("start");
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                if (b.getText().equals("stop")) {
+                    timerHandler.removeCallbacks(timerRunnable);
+                    b.setText("start");
+                } else {
+                    startTime = System.currentTimeMillis();
+                    timerHandler.postDelayed(timerRunnable, 0);
+                    b.setText("stop");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        timerHandler.removeCallbacks(timerRunnable);
+        Button b = (Button)findViewById(R.id.startButton);
+        b.setText("start");
+    }
+}
+
