b0f8e121896bf3e5daa39be8abd6595ec18b4bba
Michael Schennum <mikeschenphoto@gmail.com>
2016-06-10 13:39:27 -0700

    optimize imports

--- app/src/main/java/com/mikeschen/www/hangboardrepeaters/MainActivity.java
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/MainActivity.java
@@ -2,11 +2,10 @@ package com.mikeschen.www.hangboardrepeaters;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
