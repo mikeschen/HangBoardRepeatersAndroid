c3bf80aff45f70bde6be24f85087775d77c16b43
Michael Schennum <mikeschenphoto@gmail.com>
2016-07-02 18:40:54 -0700

    adds smaller font instructions

--- app/src/main/java/com/mikeschen/www/hangboardrepeaters/InstructionActivity.java
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/InstructionActivity.java
@@ -1,7 +1,9 @@
 package com.mikeschen.www.hangboardrepeaters;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class InstructionActivity extends AppCompatActivity {
 
