62f1e2917e87a452c90c1620920b0f6d91d0c411
Michael Schennum <mikeschenphoto@gmail.com>
2016-07-20 20:56:28 -0600

    fixes indent

--- app/src/main/java/com/mikeschen/www/hangboardrepeaters/TimerActivity.java
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/TimerActivity.java
@@ -50,68 +50,68 @@ public class TimerActivity extends AppCompatActivity {
 
         @Override
         public void run() {
-            long millis = System.currentTimeMillis() - startTime;
-            int seconds = (int) (millis / 1000);
-            int secondsDisplay = (int) (millis / 1000);
-            int minutes = secondsDisplay / 60;
-            secondsDisplay = seconds % 60;
-            if (seconds == currentTimer) {
-                timerTextView.setText(String.format("%d:%02d", 0, 0));
-                timerText.animate()
-                    .alpha(0.3f)
-                    .scaleX(0.9f)
-                    .scaleY(0.9f)
-                    .setDuration(500);
-                timerTextView.animate()
-                    .alpha(0.3f)
-                    .scaleX(0.9f)
-                    .scaleY(0.9f)
-                    .setDuration(500);
-                timerHandler.removeCallbacks(timerRunnable);
-                timerHandler.postDelayed(this, 500);
-                startTime = System.currentTimeMillis();
-                i++;
-                if (flipState) {
-                    currentTimer = pause;
-                    timerText = mPauseTextView;
-                    timerTextView = mPauseText;
-                    flipState = false;
-                } else {
-                    currentTimer = hang;
-                    timerText = mHangTextView;
-                    timerTextView = mHangText;
-                    flipState = true;
-                    mRoundsText.setText(roundCounter + "/" + rounds);
-                    roundCounter++;
-                }
-                if(i == rounds * 2 - 1) {
-                    roundCounter = 1;
-                    mRoundsText.setText(roundCounter + "/" + rounds);
-                    currentTimer = rest ;
-                    timerTextView = mRestText;
-                    timerText = mRestTextView;
-                    i = -1;
-                    mSetsText.setText(counter + "/" + sets);
-                    counter++;
-                    flipState = false;
-                }
-                if(counter == sets + 2) {
-                    timerHandler.removeCallbacks(timerRunnable);
-                    mSetsText.setText("DONE");
-                    b.setText("stop");
-                }
+        long millis = System.currentTimeMillis() - startTime;
+        int seconds = (int) (millis / 1000);
+        int secondsDisplay = (int) (millis / 1000);
+        int minutes = secondsDisplay / 60;
+        secondsDisplay = seconds % 60;
+        if (seconds == currentTimer) {
+            timerTextView.setText(String.format("%d:%02d", 0, 0));
+            timerText.animate()
+                .alpha(0.3f)
+                .scaleX(0.9f)
+                .scaleY(0.9f)
+                .setDuration(500);
+            timerTextView.animate()
+                .alpha(0.3f)
+                .scaleX(0.9f)
+                .scaleY(0.9f)
+                .setDuration(500);
+            timerHandler.removeCallbacks(timerRunnable);
+            timerHandler.postDelayed(this, 500);
+            startTime = System.currentTimeMillis();
+            i++;
+            if (flipState) {
+                currentTimer = pause;
+                timerText = mPauseTextView;
+                timerTextView = mPauseText;
+                flipState = false;
             } else {
-                timerText.animate()
-                    .alpha(1f)
-                    .scaleX(1f)
-                    .scaleY(1f);
-                timerTextView.animate()
-                    .alpha(1f)
-                    .scaleX(1f)
-                    .scaleY(1f);
-                timerTextView.setText(String.format("%d:%02d", minutes, secondsDisplay));
-                timerHandler.postDelayed(this, 500);
+                currentTimer = hang;
+                timerText = mHangTextView;
+                timerTextView = mHangText;
+                flipState = true;
+                mRoundsText.setText(roundCounter + "/" + rounds);
+                roundCounter++;
             }
+            if(i == rounds * 2 - 1) {
+                roundCounter = 1;
+                mRoundsText.setText(roundCounter + "/" + rounds);
+                currentTimer = rest ;
+                timerTextView = mRestText;
+                timerText = mRestTextView;
+                i = -1;
+                mSetsText.setText(counter + "/" + sets);
+                counter++;
+                flipState = false;
+            }
+            if(counter == sets + 2) {
+                timerHandler.removeCallbacks(timerRunnable);
+                mSetsText.setText("DONE");
+                b.setText("stop");
+            }
+        } else {
+            timerText.animate()
+                .alpha(1f)
+                .scaleX(1f)
+                .scaleY(1f);
+            timerTextView.animate()
+                .alpha(1f)
+                .scaleX(1f)
+                .scaleY(1f);
+            timerTextView.setText(String.format("%d:%02d", minutes, secondsDisplay));
+            timerHandler.postDelayed(this, 500);
+        }
         }
     };
 
@@ -174,31 +174,31 @@ public class TimerActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                final Button b = (Button) v;
-                if (newWorkoutSwitch) {
-                    new CountDownTimer(3000, 900) {
-                        public void onTick(long millisUntilFinished) {
-                            b.setText("Get Ready!  " + millisUntilFinished / 1000);
-                        }
+            final Button b = (Button) v;
+            if (newWorkoutSwitch) {
+                new CountDownTimer(3000, 900) {
+                    public void onTick(long millisUntilFinished) {
+                        b.setText("Get Ready!  " + millisUntilFinished / 1000);
+                    }
 
-                        public void onFinish() {
-                            startTime = System.currentTimeMillis();
-                            timerHandler.postDelayed(timerRunnable, 0);
-                            b.setText("stop");
-                            newWorkoutSwitch = false;
-                        }
-                    }.start();
-                }
-                if (!newWorkoutSwitch) {
-                    if (b.getText().equals("stop")) {
-                        timerHandler.removeCallbacks(timerRunnable);
-                        b.setText("start");
-                    } else {
+                    public void onFinish() {
                         startTime = System.currentTimeMillis();
                         timerHandler.postDelayed(timerRunnable, 0);
                         b.setText("stop");
+                        newWorkoutSwitch = false;
                     }
+                }.start();
+            }
+            if (!newWorkoutSwitch) {
+                if (b.getText().equals("stop")) {
+                    timerHandler.removeCallbacks(timerRunnable);
+                    b.setText("start");
+                } else {
+                    startTime = System.currentTimeMillis();
+                    timerHandler.postDelayed(timerRunnable, 0);
+                    b.setText("stop");
                 }
+            }
             }
         });
     }
