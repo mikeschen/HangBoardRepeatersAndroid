3cb6a956815ea46e33f31c01ad6c67ca3a4617d7
Michael Schennum <mikeschenphoto@gmail.com>
2016-07-26 14:19:33 -0700

    preparre to merge

--- app/src/main/java/com/mikeschen/www/hangboardrepeaters/MainActivity.java
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/MainActivity.java
@@ -14,7 +14,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
