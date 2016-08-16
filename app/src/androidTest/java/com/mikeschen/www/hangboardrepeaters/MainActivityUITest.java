05b1019a0129ffcc984af01077524deca63805c5
Michael Schennum <mikeschenphoto@gmail.com>
2016-08-16 10:05:13 -0700

    adds ui espresso test

--- app/src/androidTest/java/com/mikeschen/www/hangboardrepeaters/MainActivityUITest.java
+++ app/src/androidTest/java/com/mikeschen/www/hangboardrepeaters/MainActivityUITest.java
@@ -7,6 +7,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.notNullValue;
+
 @RunWith(AndroidJUnit4.class)
 public class MainActivityUITest {
 
@@ -15,6 +22,8 @@ public class MainActivityUITest {
 
     @Test
     public void buttonLaunchesOtherActivity() throws Exception {
-
+        String otherActivityString = "Hang";
+        onView(withId(R.id.startButton)).perform(click());
+        onView(withText(otherActivityString)).check(matches(notNullValue()));
     }
 }
