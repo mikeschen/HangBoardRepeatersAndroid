75c803a38367de16c0899564124d2493ac2ba557
Michael Schennum <mikeschenphoto@gmail.com>
2016-10-04 17:17:21 -0700

    adds delete button

--- app/src/main/java/com/mikeschen/www/hangboardrepeaters/LogActivity.java
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/LogActivity.java
@@ -1,9 +1,13 @@
 package com.mikeschen.www.hangboardrepeaters;
 
 import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,7 +16,7 @@ import com.mikeschen.www.hangboardrepeaters.Models.Days;
 
 import java.util.List;
 
-public class LogActivity extends ListActivity {
+public class LogActivity extends ListActivity implements View.OnClickListener {
     private DaysDataSource datasource;
 
     @Override
@@ -32,5 +36,39 @@ public class LogActivity extends ListActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case(R.id.deleteButton) :
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setTitle("Delete Data");
+                builder.setMessage("Are you sure you want to delete your history?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        db.deleteAllDayRecords();
+                        db.deleteAllFoodRecords();
+                        db.closeDB();
+                        refresh();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.show();
+        }
+    }
+    public void refresh() {
+        Intent intent = new Intent(LogActivity.this,LogActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
