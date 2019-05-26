725acfc3900f563f68449150d999e7b6657c9762
Michael Schennum <mschennum@ackmanndickenson.com>
2019-05-25 19:05:30 -0700

    youtube

---
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/Config.java
@@ -0,0 +1,10 @@
+package com.mikeschen.www.hangboardrepeaters;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCz36hm8QeLKV2ZPmkbQQWiRZ3RJNzN1L8";
+
+}
--- app/src/main/java/com/mikeschen/www/hangboardrepeaters/InstructionActivity.java
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/InstructionActivity.java
@@ -2,12 +2,57 @@ package com.mikeschen.www.hangboardrepeaters;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;import com.mikeschen.www.hangboardrepeaters.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
 
-public class InstructionActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class InstructionActivity extends YouTubeBaseActivity implements
+    YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo("fhWaJi1Hsfo"); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, this);
+        }
+    }
+
+    protected Provider getYouTubePlayerProvider() {
+        return youTubeView;
     }
 }
