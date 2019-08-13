a8478964d0b40389b99802d7104c669b365a8251
Michael Schennum <mschennum@ackmanndickenson.com>
2019-08-13 08:07:57 -0700

    change constants names

--- app/src/main/java/com/mikeschen/www/hangboardrepeaters/Constants.java
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/Constants.java
@@ -8,9 +8,9 @@ public class Constants {
     public static final String KEY_USER_SETS = "sets";
     public static final String KEY_USER_SIZE = "size";
     public static final String KEY_USER_WEIGHT = "weight";
-    public static final String SAVED_USER_HANG = "hang";
-    public static final String SAVED_USER_PAUSE = "pause";
-    public static final String SAVED_USER_ROUNDS = "rounds";
-    public static final String SAVED_USER_REST = "rest";
-    public static final String SAVED_USER_SETS = "sets";
+    public static final String SAVED_USER_HANG = "savedHang";
+    public static final String SAVED_USER_PAUSE = "savedPause";
+    public static final String SAVED_USER_ROUNDS = "savedRounds";
+    public static final String SAVED_USER_REST = "savedRest";
+    public static final String SAVED_USER_SETS = "savedSets";
 }
