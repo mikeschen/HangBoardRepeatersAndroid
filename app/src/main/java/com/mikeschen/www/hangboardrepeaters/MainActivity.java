f63fad31b0d43e80f0459f40b4b02f2f98e72bfe
Michael Schennum <mschennum@ackmanndickenson.com>
2019-08-14 09:12:38 -0700

    new icon

--- app/src/main/java/com/mikeschen/www/hangboardrepeaters/MainActivity.java
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/MainActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
