3bc573192af45cf7cf59684220503dce6b03fdb5
Michael Schennum <mikeschenphoto@gmail.com>
2020-08-24 10:49:04 -0700

    demo countdown timer button

--- app/src/main/java/com/mikeschen/www/hangboardrepeaters/TimerActivity.java
+++ app/src/main/java/com/mikeschen/www/hangboardrepeaters/TimerActivity.java
@@ -20,6 +20,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.os.Build;
+import android.widget.Toast;
 
 import com.mikeschen.www.hangboardrepeaters.Presenters.TimerActivityPresenter;
 import com.mikeschen.www.hangboardrepeaters.Views.TimerActivityView;
@@ -40,6 +41,7 @@ public class TimerActivity extends AppCompatActivity implements TimerActivityVie
     @BindView(R.id.roundsText) TextView mRoundsText;
     @BindView(R.id.startButton) Button mStartButton;
     @BindView(R.id.soundButton) ImageButton mSoundButton;
+    @BindView(R.id.countSoundButton) ImageButton mCountSoundButton;
 
     TextView timerText;
     TextView timerTextView;
@@ -56,6 +58,7 @@ public class TimerActivity extends AppCompatActivity implements TimerActivityVie
     boolean newWorkoutSwitch = true;
     boolean flipState = true;
     boolean soundSwitch = true;
+    boolean countSoundSwitch = false;
     int buttonchimeId;
     int pausechimeId;
     int restwarningId;
@@ -78,9 +81,9 @@ public class TimerActivity extends AppCompatActivity implements TimerActivityVie
             int minutes = countdownDisplay / 60;
             int secondsDisplay = countdownDisplay % 60;
 //            Log.d(TAG, "run: " + countdownDisplay);
-            if (soundSwitch) {
+            if (countSoundSwitch) {
                 if (countdownDisplay == 5) {
-                    ourSounds.play(fivesecondsId, 0.9f, 0.9f, 1, 0, 1);
+                    ourSounds.play(fivesecondsId, 0.8f, 0.8f, 1, 0, 1);
                 }
             }
             if (seconds == currentTimer) {
@@ -159,6 +162,7 @@ public class TimerActivity extends AppCompatActivity implements TimerActivityVie
 
         mStartButton.setOnClickListener(this);
         mSoundButton.setOnClickListener(this);
+        mCountSoundButton.setOnClickListener(this);
         Typeface custom_font = Typeface.createFromAsset(getAssets(), "BebasNeue.ttf");
         mHangTextView.setTypeface(custom_font);
         mPauseTextView.setTypeface(custom_font);
@@ -247,9 +251,38 @@ public class TimerActivity extends AppCompatActivity implements TimerActivityVie
                 if (soundSwitch) {
                     soundSwitch = false;
                     mSoundButton.setImageResource(R.drawable.ic_volume_off_white_36dp);
+                    Toast soundOff = Toast.makeText(getApplicationContext(),
+                            "Sound Off",
+                            Toast.LENGTH_SHORT);
+
+                    soundOff.show();
                 } else {
                     soundSwitch = true;
                     mSoundButton.setImageResource(R.drawable.ic_volume_up_white_36dp);
+                    Toast soundOn = Toast.makeText(getApplicationContext(),
+                            "Sound On",
+                            Toast.LENGTH_SHORT);
+
+                    soundOn.show();
+                }
+                break;
+            case (R.id.countSoundButton):
+                if (countSoundSwitch) {
+                    countSoundSwitch = false;
+                    mCountSoundButton.setImageResource(R.drawable.baseline_timer_off_white_36dp);
+                    Toast cntSoundOff = Toast.makeText(getApplicationContext(),
+                            "Countdown Sounds Off",
+                            Toast.LENGTH_SHORT);
+
+                    cntSoundOff.show();
+                } else {
+                    countSoundSwitch = true;
+                    mCountSoundButton.setImageResource(R.drawable.baseline_av_timer_white_36dp);
+                    Toast cntSoundOn = Toast.makeText(getApplicationContext(),
+                            "Countdown Sounds On",
+                            Toast.LENGTH_SHORT);
+
+                    cntSoundOn.show();
                 }
                 break;
         }
